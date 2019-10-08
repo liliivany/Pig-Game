dd4c456fd3a00097f9b036ca7096ab5bf8dea63e
Lili Ivany <lilkavanilka@gmail.com>
2019-10-08 17:05:54 +0200

    add the option to display the result, update the round score if the rolled number was not a 1 and get next player if current dice was 1

--- app.js
+++ app.js
@@ -8,3 +8,39 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random number
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice > 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+  }
+});
+
+// getter var x = document.querySelector("#score-0").textContent;
